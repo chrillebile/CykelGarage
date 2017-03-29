41ee661df111d480b49c543e16da1c3147d4d9f8
chrillebile <chrillebile@gmail.com>
2017-03-29 15:00:20 +0200

    La till knappar för att stänga och lägga till en användare. (Kan dock inte lägga till användare än).

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,25 +1,40 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        /*Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();*/
+
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(e -> Platform.exit());
+
+        Button btn2 = new Button();
+        btn2.setText("Lägg till customer");
+        // See lambda expression
+        btn2.setOnAction(e -> /**Todo: Lägg till en användare*/);
+
+
+
+        HBox hbox = new HBox();
         BorderPane root = new BorderPane();
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Title");
+        hbox.getChildren().add(btn);
+        hbox.getChildren().add(btn2);
+        root.setBottom(hbox);
+        primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
+
+
     }
 
 
