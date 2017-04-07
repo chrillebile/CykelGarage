4ff2446e6fa7866a7c2139176c638bdbe3449aff
enniomara <enniomara1@gmail.com>
2017-04-07 15:03:34 +0200

    Added base methods for AdminManager & BikeManager

--- src/BikeGarage/AdminManager.java
+++ src/BikeGarage/AdminManager.java
@@ -1,4 +1,121 @@
 package BikeGarage;
 
+import java.util.ArrayList;
+
 public class AdminManager {
+    CustomerManager customerManager;
+    BikeManager bikeManager;
+    DatabaseManager dbManager;
+
+    /**
+     * This is the class that connects all major components of the garage. All administrative tasks are done by this.
+     * @param customerManager Manager for customers
+     * @param bikeManager Manager for bikes
+     * @param dbManager Manager for database
+     */
+    public AdminManager(CustomerManager customerManager, BikeManager bikeManager, DatabaseManager dbManager){
+
+    }
+
+    /**
+     * @param personNr The user's personal id.
+     * @return A list of bikes a user has.
+     */
+    public ArrayList<Bike> findBikesByCustomer(String personNr){
+
+    }
+
+    /**
+     * Create a new user with the given parameters.
+     * @param firstName
+     * @param surname
+     * @param personNr The user's personal id number.
+     * @param pin
+     * @return The created user.
+     */
+    public Customer createCustomer(String firstName, String surname, String personNr, String pin){
+
+    }
+
+    /**
+     * Remove a customer with a given personNr.
+     * @param personNr The user's personal id number.
+     * @return Whether the removal was successful.
+     */
+    public boolean removeCustomer(String personNr){
+
+    }
+
+    /**
+     * Add a new bike to a specific user. The customer is already given.
+     * @param customer The customer object.
+     * @return The created bike.
+     */
+    public Bike addBike(Customer customer){
+
+    }
+
+    /**
+     * Add a new bike to a specific user. The customer is searched by personNr.
+     * @param personNr The personal id of the user.
+     * @return The bike that has been created.
+     */
+    public Bike addBike(String personNr){
+
+    }
+
+    /**
+     * Remove a bike with a given barcodeNr.
+     * @param barcodeNr This is the bike's identification number. It is unique.
+     * @return Whether the bike has been successfully removed.
+     */
+    public boolean removeBike(String barcodeNr){
+
+    }
+
+    /**
+     * @return The list of stored customers.
+     */
+    public ArrayList<Customer> getCustomerList(){
+
+    }
+
+    /**
+     * @return The list of all stored bikes.
+     */
+    public ArrayList<Bike> getBikeList(){
+
+    }
+
+    /**
+     * Seearch a specific customer given its personNr.
+     * @param personNr Unique identification for the customer
+     * @return The found customer.
+     */
+    public Customer findCustomer(String personNr){
+
+    }
+
+    /**
+     * Search a specific bike given its barcodeNr.
+     * @param barcodeNr Unique identification for the bike.
+     * @return The found bike.
+     */
+    public Bike findBike(String barcodeNr){
+
+    }
+
+    /**
+     * @return Number of free parking spots.
+     */
+    public int numberOfFreeParkingSpots(){
+
+    }
+
+    /**
+     * @return Total number of parking spots the garage has.
+     */
+    public int totalNumberOfParkingSpots(){
+
+    }
 }
--- src/BikeGarage/BikeManager.java
+++ src/BikeGarage/BikeManager.java
@@ -1,4 +1,97 @@
 package BikeGarage;
 
+import java.util.ArrayList;
+
 public class BikeManager {
+    private ArrayList<Bike>  bikeList;
+
+    /**
+     * The manager that manages bikes registered in the garage.
+     */
+    public BikeManager(){
+    }
+
+    /**
+     * Search after a bike that is owned by a specific user.
+     * @param personNr The user's personal identification number.
+     * @return The found bike.
+     */
+    public Bike findBikeByPersonNr(String personNr){
+
+    }
+
+    /**
+     * Search after a bike given its barcode number.
+     * @param barcodeNr Unique identification for the bike.
+     * @return The found bike.
+     */
+    public Bike findBikeByBarcodeNr(String barcodeNr){
+
+    }
+
+    /**
+     * @return A list of all bikes stored in the system.
+     */
+    public ArrayList<Bike> getBikeList(){
+
+    }
+
+    /**
+     * Add a bike to a given customer.
+     * @param customer The customer where the bike will be added.
+     * @return The complete bike object, including the customer.
+     */
+    public Bike addBike(Customer customer){
+
+    }
+
+    /**
+     * Remove a bike with a specific barcode.
+     * @param barcodeNr Unique identification for the bike.
+     * @return Whether the removal was successful.
+     */
+    public boolean removeBike(String barcodeNr){
+
+    }
+
+    /**
+     * Edit a bike's customer.
+     * @param barcodeNr The bike.
+     * @param newCustomer The bike's customer.
+     * @return Whether the edit was successful.
+     */
+    public boolean editBikeCustomer(String barcodeNr, Customer newCustomer){
+
+    }
+
+    /**
+     * Set a bike's entry time.
+     * @param barcodeNr Unique identification for the bike.
+     * @param entryTime The time specified in unix time.
+     * @return Whether the edit was successful.
+     */
+    public boolean setBikeEntryTime(String barcodeNr, long entryTime){
+
+    }
+
+    /**
+     * Set a bike's exit time.
+     * @param barcodenr Unique identification for the bike.
+     * @param exitTime The exit time specified in unix time.
+     * @return Whether the edit was successful.
+     */
+    public boolean setBikeExitTime(String barcodenr, long exitTime){
+
+    }
+
+    /**
+     * Check if a given bike is parked in the garage.
+     * @param barcodeNr Unique identification for the bike.
+     * @return Whether the bike is parked.
+     */
+    public boolean isBikeParked(String barcodeNr){
+
+    }
+
+
 }
