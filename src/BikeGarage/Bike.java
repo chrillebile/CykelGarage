95d537ca05fcc15c591552d287f0df5b139b2ae3
chrillebile <chrillebile@gmail.com>
2017-04-07 15:03:49 +0200

    Added base methods for Bike and CustomerManager

--- src/BikeGarage/Bike.java
+++ src/BikeGarage/Bike.java
@@ -1,4 +1,98 @@
 package BikeGarage;
 
 public class Bike {
+
+    private Customer customer;
+    private String barcodeNr;
+    private long regTime, entryTime, exitTime;
+
+    /**
+     * Constructor for bikes.
+     * @param barcodeNr The assigned unique barcode the bike should have.
+     * @param customer The bike's owner.
+     */
+    public Bike(String barcodeNr, Customer customer){
+
+    }
+
+    /**
+     * Constructor for bikes. This is primarily used by the database to create bikes during startup.
+     * @param customer The owner.
+     * @param barcodeNr The unique barcode.
+     * @param regTime Time when the bike was registered.
+     * @param entryTime When the bike last entered the garage.
+     * @param exitTime When the bike last exited the
+     */
+    public Bike(Customer customer, String barcodeNr, long regTime, long entryTime, long exitTime){
+
+    }
+
+    /**
+     *
+     * @return The bike's customer
+     */
+    public Customer getCustomer(){
+        return null;
+    }
+
+    /**
+     * @return The bike's barcodeNr
+     */
+    public String getBarcodeNr(){
+        return null;
+    }
+
+    /**
+     * @return The bike's registration time
+     */
+    public long getRegTime(){
+        return 0;
+    }
+
+    /**
+     * @return Time of entry
+     */
+    public long getEntryTime(){
+        return 0;
+    }
+
+    /**
+     * @return Time of exit
+     */
+    public long getExitTime(){
+        return 0;
+    }
+
+    /**
+     * Replace the bike's customer
+     * @param customer
+     */
+    public void setCustomer(Customer customer){
+
+    }
+
+    /**
+     * Set time of entry in the garage
+     * @param entryTime
+     */
+    public void setEntryTime(long entryTime){
+
+    }
+
+    /**
+     * Set time of exit in the garage
+     * @param exitTime
+     */
+    public void setExitTime(long exitTime){
+
+    }
+
+    /**
+     * Checks if object has same barcode as this bike
+     * @param object
+     * @return
+     */
+    public boolean equals(Object object){
+        return false;
+    }
 }
--- src/BikeGarage/CustomerManager.java
+++ src/BikeGarage/CustomerManager.java
@@ -1,5 +1,83 @@
 package BikeGarage;
 
+import java.util.ArrayList;
+
 public class CustomerManager {
 
+    private ArrayList<Customer> customerList;
+
+    /**
+     * New Customer manager
+     */
+    public CustomerManager(){
+        customerList = new ArrayList<Customer>();
+    }
+
+    /**
+     * Returns a customer if he exist
+     * @param personNr
+     * @return
+     */
+    public Customer findCustomerByPersonNr(String personNr){
+        return null;
+    }
+
+    /**
+     * Check if a pin exist
+     * @param pin
+     * @return
+     */
+    public boolean checkIfPinExist(String pin){
+        return false;
+    }
+
+    /**
+     * Returns alla customers as an ArrayList
+     * @return
+     */
+    public ArrayList<Customer> getCustomerList(){
+        return null;
+    }
+
+    /**
+     * Create new customer if he don't already exist.
+     * @param firstName
+     * @param surname
+     * @param personNr
+     * @param pin
+     * @return
+     */
+    public boolean createCustomer(String firstName, String surname, String personNr, String pin){
+        return false;
+    }
+
+    /**
+     * Removes a customer if he exist
+     * @param personNr
+     * @return
+     */
+    public boolean removeCustomer(String personNr){
+        return false;
+    }
+
+    /**
+     * Edit customers name if he exist
+     * @param personNr
+     * @param firsName
+     * @param surname
+     * @return
+     */
+    public boolean editCustomerName(String personNr, String firsName, String surname){
+        return false;
+    }
+
+    /**
+     * Edit customers pin if he exist
+     * @param personNr
+     * @param pin
+     * @return
+     */
+    public boolean editCustomerPin(String personNr, String pin){
+        return false;
+    }
 }
