6b33ee2190f4a7cfb20437feee153af24719bc45
chrillebile <chrillebile@gmail.com>
2017-04-24 10:04:06 +0200

    Added methods of DatabaseManager

--- src/BikeGarage/DatabaseManager.java
+++ src/BikeGarage/DatabaseManager.java
@@ -1,4 +1,31 @@
 package BikeGarage;
 
 public class DatabaseManager {
+
+    public DatabaseManager(){
+    }
+
+    public BikeManager loadBikes(){
+        return null;
+    }
+
+    public CustomerManager loadCustomers(){
+        return null;
+    }
+
+    public Config loadConfig(){
+        return null;
+    }
+
+    public void updateBikes(){
+
+    }
+
+    public void updateCustomers(){
+
+    }
+
+    public void updateConfig(){
+
+    }
 }
