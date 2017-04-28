61b4aee2ba244370f9f95bc4831759c1688c1791
enniomara <enniomara1@gmail.com>
2017-04-29 01:48:49 +0200

    WindowManager now can create the different views

--- src/BikeGarage/WindowManager.java
+++ src/BikeGarage/WindowManager.java
@@ -1,4 +1,73 @@
 package BikeGarage;
 
+import BikeGarage.controllers.MainController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class WindowManager {
+
+    private CustomerManager customerManager;
+    private BikeManager bikeManager;
+
+    public WindowManager(CustomerManager customerManager, BikeManager bikeManager){
+        this.customerManager = customerManager;
+        this.bikeManager = bikeManager;
+    }
+
+    public void initMain(){
+        Stage stage = new Stage();
+
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("views/main.fxml"));
+            AnchorPane root = loader.load();
+
+            stage.setScene(new Scene(root));
+
+            MainController controller = loader.getController();
+            controller.setWindowManager(this);
+
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void initEditUser(){
+        Stage stage = new Stage();
+
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("views/editUser.fxml"));
+            AnchorPane root = loader.load();
+
+            stage.setScene(new Scene(root));
+
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void initEditBike(){
+        Stage stage = new Stage();
+
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("views/editBike.fxml"));
+            AnchorPane root = loader.load();
+
+            stage.setScene(new Scene(root));
+
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
